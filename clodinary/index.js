e80f1dfea065f4e905830bb2b927c9c4b78fc7fc
Ashesh Mandal <116034358+asheshmandal2003@users.noreply.github.com>
2023-08-07 23:46:07 +0530

    Added cloud storage

---
+++ clodinary/index.js
@@ -0,0 +1,18 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "CareData",
+    allowedFormats: ["jpeg", "png", "jpg", "pdf"],
+  },
+});
+
+module.exports = { cloudinary, storage };
